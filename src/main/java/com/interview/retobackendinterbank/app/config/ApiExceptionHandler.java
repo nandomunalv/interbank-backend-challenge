7c0f8fff6afb9c785b4fa5ed3c4c939795426c26
Fernando <nando.munalv@gmail.com>
2022-09-16 23:09:19 -0500

    feat: se añadió una excepción para controlar la validación de las dto

--- src/main/java/com/interview/retobackendinterbank/app/config/ApiExceptionHandler.java
+++ src/main/java/com/interview/retobackendinterbank/app/config/ApiExceptionHandler.java
@@ -2,11 +2,15 @@ package com.interview.retobackendinterbank.app.config;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
@@ -16,5 +20,16 @@ public class ApiExceptionHandler {
         ApiException apiException = new ApiException(e.getMessage(), HttpStatus.BAD_REQUEST,
                 ZonedDateTime.now(ZoneId.of("America/Lima")));
         return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
